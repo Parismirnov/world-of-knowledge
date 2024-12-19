6a8552bf1ee1aa6ff1bbc8c586b4a397bd53452f
Brian Emanuel Morales Mora <parismirnov@gmail.com>
2024-12-18 21:12:39 -0600

    se crea un titulo

--- biome1/biome1.js
+++ biome1/biome1.js
@@ -1,5 +1,16 @@
 let section = document.createElement('div');
 section.className = "section-c1"
 
+let h1 = document.createElement('h1');
+h1.className = "titulo-b1";
+h1.innerText = "WELCOME TO THE";
+section.appendChild(h1);
+
+let h2 = document.createElement('h1');
+h2.className = "titulo2-b1";
+h2.innerText = "world of knowledge";
+section.appendChild(h2);
+
+
 
 export{section};
