4125f65497064948aaca6686909067721bd227d6
Brian Morales <parismirnov@gmail.com>
2025-02-11 17:21:58 -0600

    se crea crafts

---
+++ tutoriales/crafts.js
@@ -0,0 +1,21 @@
+let body = document.body;
+body.className = "crafts1"
+
+let contenedor1_c = document.createElement('div');
+contenedor1_c.className = "contenedor1-c"
+contenedor1_c.innerHTML = `
+<h1>crafting tutorials</h1>
+
+`;
+body.appendChild(contenedor1_c);
+
+let contenedor2_c = document.createElement('div');
+contenedor2_c.className = "contenedor2-c"
+contenedor2_c.innerHTML = `
+<div class="mesa"><img src="https://i.pinimg.com/1200x/20/0b/f6/200bf6ee3a7bb2e802ddf466ec4bf9ad.jpg" alt=""></div>
+`;
+body.appendChild(contenedor2_c);
+
+let contenedor3_c = document.createElement('div');
+contenedor3_c.className = "contenedor3-c"
+body.appendChild(contenedor3_c);
